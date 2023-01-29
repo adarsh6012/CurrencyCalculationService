2121401b0a4d728c359dbeb0701d33a8c21ef0a8
Adarsh Kumar <adarsh>
2023-01-29 16:26:11 +0530

    first commit

---
+++ src/main/java/com/codefest/currencycalculation/CurrencyCalculationServiceApplication.java
@@ -0,0 +1,16 @@
+package com.codefest.currencycalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+
+public class CurrencyCalculationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyCalculationServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/codefest/currencycalculation/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.codefest.currencycalculation;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CurrencyCalculationServiceApplication.class);
+	}
+
+}
